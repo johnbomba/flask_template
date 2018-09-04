2eb7e518e72330c505454dd7183108987f6d02f2
John Bomba <bomba.john@gmail.com>
2018-09-04 13:27:50 -0400

    ready for collab

--- hello.py
+++ hello.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template 
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+Bootstrap(app)
+
 @app.route("/")
+@app.route("/index")
 def index():
-    return "hello world"
+    return render_template("template.html")
+
+
 
 if __name__ == "__main__":
     app.run("127.0.0.1", debug= True)
